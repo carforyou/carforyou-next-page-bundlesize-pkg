46f8f39dfb499d459e94bb6c4e26f520608d8fa5
Christian Felder (masone) <ema@rh-productions.ch>
2021-07-03 10:17:17 +0200

    fix: lint

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,28 +1,54 @@
-import run from "../index"
 import fs from "fs"
+
+import run from "../index"
 
 describe("cli", () => {
   it("produces config with correct files and sizes", () => {
-    const mockExit = jest.spyOn(process, 'exit').mockImplementation()
-    run(["jest", "./node_modules/.bin/jest", "--maxSize", "1 kB", "--buildDir", "./src/__tests__/.next"])
+    const mockExit = jest.spyOn(process, "exit").mockImplementation()
+    run([
+      "jest",
+      "./node_modules/.bin/jest",
+      "--maxSize",
+      "1 kB",
+      "--buildDir",
+      "./src/__tests__/.next",
+    ])
 
-    const config = fs.readFileSync("./src/__tests__/.next/next-page-bundlesize.config.json").toString()
+    const config = fs
+      .readFileSync("./src/__tests__/.next/next-page-bundlesize.config.json")
+      .toString()
     expect(config).toMatchSnapshot()
     expect(mockExit).toHaveBeenCalledWith(0)
   })
 
   it("produces concatenated bundle file", () => {
-    const mockExit = jest.spyOn(process, 'exit').mockImplementation()
-    run(["jest", "./node_modules/.bin/jest", "--maxSize", "1 kB", "--buildDir", "./src/__tests__/.next"])
+    const mockExit = jest.spyOn(process, "exit").mockImplementation()
+    run([
+      "jest",
+      "./node_modules/.bin/jest",
+      "--maxSize",
+      "1 kB",
+      "--buildDir",
+      "./src/__tests__/.next",
+    ])
 
-    const page = fs.readFileSync("./src/__tests__/.next/.bundlesize_").toString()
+    const page = fs
+      .readFileSync("./src/__tests__/.next/.bundlesize_")
+      .toString()
     expect(page).toMatchSnapshot()
     expect(mockExit).toHaveBeenCalledWith(0)
   })
 
   it("fails when bundles are larger than the limit", () => {
-    const mockExit = jest.spyOn(process, 'exit').mockImplementation()
-    run(["jest", "./node_modules/.bin/jest", "--maxSize", "1 b", "--buildDir", "./src/__tests__/.next"])
+    const mockExit = jest.spyOn(process, "exit").mockImplementation()
+    run([
+      "jest",
+      "./node_modules/.bin/jest",
+      "--maxSize",
+      "1 b",
+      "--buildDir",
+      "./src/__tests__/.next",
+    ])
     expect(mockExit).toHaveBeenCalledWith(1)
   })
 })
