451196d735618eaf99eb489448fb65baaf95c138
Christian Felder (masone) <ema@rh-productions.ch>
2022-01-12 14:05:41 +0100

    fix: tests

---
+++ src/__tests__/next-page-bundlesize.test.ts
@@ -0,0 +1,77 @@
+import fs from "fs"
+
+import run from "../../index"
+
+const validRunConfig = [
+  "jest",
+  "./node_modules/.bin/jest",
+  "--maxSize",
+  "1 kB",
+  "--buildDir",
+  "./src/__tests__/.next",
+]
+
+describe("cli", () => {
+  const mockExit = jest.spyOn(process, "exit").mockImplementation()
+
+  beforeEach(() => {
+    jest.resetModules()
+    jest.clearAllMocks()
+  })
+
+  it("produces config with correct files and sizes", () => {
+    run(validRunConfig)
+
+    const config = fs
+      .readFileSync("./src/__tests__/.next/next-page-bundlesize.config.json")
+      .toString()
+    expect(config).toMatchSnapshot()
+    expect(mockExit).toHaveBeenCalledWith(0)
+  })
+
+  it("produces concatenated bundle file", () => {
+    run(validRunConfig)
+
+    const page = fs
+      .readFileSync("./src/__tests__/.next/.bundlesize_")
+      .toString()
+    expect(page).toMatchSnapshot()
+    expect(mockExit).toHaveBeenCalledWith(0)
+  })
+
+  it("fails when bundles are larger than the limit", () => {
+    run([
+      "jest",
+      "./node_modules/.bin/jest",
+      "--maxSize",
+      "1 b",
+      "--buildDir",
+      "./src/__tests__/.next",
+    ])
+    expect(mockExit).toHaveBeenCalledWith(1)
+  })
+
+  it("uses the previous config if defined", () => {
+    run([...validRunConfig, "--previousConfigFileName", "master-config.json"])
+
+    const updatedConfig = fs
+      .readFileSync("./src/__tests__/.next/bundlesize.json")
+      .toString()
+    expect(updatedConfig).toMatchSnapshot()
+  })
+
+  it("adds a delta to the new config if smaller than maxSize", () => {
+    run([
+      ...validRunConfig,
+      "--previousConfigFileName",
+      "master-config.json",
+      "--delta",
+      "5 kB",
+    ])
+
+    const updatedConfig = fs
+      .readFileSync("./src/__tests__/.next/bundlesize.json")
+      .toString()
+    expect(updatedConfig).toMatchSnapshot()
+  })
+})
