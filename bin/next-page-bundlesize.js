1bd47dfacd24de21ea440f3ee7f4449e3644810a
Christian Felder (masone) <ema@rh-productions.ch>
2022-01-12 13:13:50 +0100

    fix: move

--- bin/next-page-bundlesize.js
+++ bin/next-page-bundlesize.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
+'use strict'
 
 // eslint-disable-next-line import/no-internal-modules
-import run from "../pkg/cjs"
-run()
+const run = require("../pkg/cjs").default
+
+try {
+  run(process.argv)
+} catch (err) {
+  // eslint-disable-next-line no-console
+  console.log(err)
+  process.exit(1)
+}
+
+process.exit(0)
